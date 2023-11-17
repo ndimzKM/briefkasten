4a6fafd55e033499dc905842fc79712e454a1e6e
Alieu Ndimbalane <alieundimbalane@gmail.com>
2023-11-17 15:28:32 +0000

    restore defaults

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -98,7 +98,7 @@ const Signin = ({ providers, csrfToken, autoLoginFirstProvider }) => {
   )
 
   if (autoLoginFirstProvider && !query.error) {
-    signIn(providers[0], null, {login_hint: "ndimz2019@gmail.com"});
+    signIn(providers[0]);
   }
 
   return (
